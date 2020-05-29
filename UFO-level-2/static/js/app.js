4b9c628a6c12cb2b13c39a780794deb81432ad52
yiran hong <yiranh37@gmail.com>
2020-05-28 17:40:40 -0700

    updated js file

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -49,38 +49,3 @@ function filterData( ){
         });
     });
 };
-
-
-
-
-/*  
-    filterList.forEach(([key, value]) => {
-        d3.select("tbody").text("");
-        d3.selectAll("li");    
-        //var row = select("tbody").append("tr");
-        //var cell = row.append("td");
-        //cell.text(value);
-        console.log(filteredResult);
-    });
-}
-*/
-
-/*
-function loadData (parameter) {
-    //d3.event.preventDefault();
-    d3.select("tbody").text("");
-    d3.selectAll("li");
-
-    var result = filterData.filter(row => row.inputID===row.inputValue);
-    console.log(result);
-    resultRow = result.forEach(element => {
-        var row = d3.select("tbody").append("tr");
-        Object.entries(element).forEach(([key, value])=>{
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });  
-}; 
-
-//loadData(filter);
-*/
